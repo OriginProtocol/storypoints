b9bf6a3b5ac5fe9b5a0ae0020c2c66df64194048
Mike Shultz <shultzm@gmail.com>
2023-05-02 16:29:59 -0600

    add 20230429180333-add-activity-context.js migration

--- packages/models/src/migrate.ts
+++ packages/models/src/migrate.ts
@@ -5,6 +5,7 @@ import { SequelizeStorage, Umzug } from 'umzug'
 import * as createActivity from './migrations/20230406150645-create-activity'
 import * as createCollection from './migrations/20230424200130-create-collection'
 import * as createWallet from './migrations/20230428184115-create-wallet'
+import * as addContextBlob from './migrations/20230429180333-add-activity-context'
 import * as addReservoirOrderId from './migrations/20230502021736-add-reservoir-order-id'
 import { QueryInterface, sequelize } from './models'
 
@@ -17,6 +18,7 @@ const migrations = {
   ['20230406150645-create-activity.js']: createActivity,
   ['20230424200130-create-collection.js']: createCollection,
   ['20230428184115-create-wallet.js']: createWallet,
+  ['20230429180333-add-activity-context.js']: addContextBlob,
   ['20230502021736-add-reservoir-order-id.js']: addReservoirOrderId,
 }
 
