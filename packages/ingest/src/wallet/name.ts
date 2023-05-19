21c4f982a6f7f06504e81a584b3ac50f4abd4648
Mike Shultz <shultzm@gmail.com>
2023-05-19 14:45:17 -0600

    add error handling for resolveENS()

--- packages/ingest/src/wallet/name.ts
+++ packages/ingest/src/wallet/name.ts
@@ -1,10 +1,18 @@
-import { address } from '@storypoints/utils'
+import { address, logger } from '@storypoints/utils'
 import { getMainnetProvider } from '@storypoints/utils/eth'
+
+const log = logger.child({ app: 'ingest', module: 'wallet' })
 
 /// Resolve ENS name
 export async function resolveENS(
   walletAddress: Buffer | string
 ): Promise<string | null> {
-  const provider = getMainnetProvider()
-  return await provider.lookupAddress(address(walletAddress))
+  try {
+    const provider = getMainnetProvider()
+    return await provider.lookupAddress(address(walletAddress))
+  } catch (err) {
+    log.error(err, `Error resolving name for address ${address(walletAddress)}`)
+  }
+
+  return null
 }
